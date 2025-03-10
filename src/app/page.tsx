93dcbdf3482df6ec85f53124a4d8f9d9da3d2db3
Vitor Andrey <vitor.andreylopes@gmail.com>
2025-03-10 15:59:55 -0300

    fix: responsiviness of building screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4">
+    <div className="flex h-screen w-full flex-col text-center p-4 items-center justify-center gap-4">
       <h1 className="text-5xl text-zinc-800">🏗️ Em construção...</h1>
 
       <p className="text-sm text-zinc-500">
