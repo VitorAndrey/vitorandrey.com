fb2f3b4530dd63de070f1b647086723b73b35d7e
Vitor Andrey <vitor.andreylopes@gmail.com>
2025-03-17 18:09:43 -0300

    chore: frammer motion

--- src/app/components/sections/hero-section.tsx
+++ src/app/components/sections/hero-section.tsx
@@ -1,3 +1,36 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export function HeroSection() {
-  return <div className="section">HeroSection</div>;
+  return (
+    <div className="section flex justify-center items-center min-h-screen bg-gray-100">
+      <motion.div
+        className="hero_image"
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{
+          borderRadius: [
+            "60% 40% 30% 70% / 60% 30% 70% 40%",
+            "30% 60% 70% 40% / 50% 60% 30% 60%",
+            "60% 40% 30% 70% / 60% 30% 70% 40%",
+          ],
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          borderRadius: {
+            duration: 8,
+            ease: "easeInOut",
+            repeat: Infinity,
+          },
+          opacity: {
+            duration: 1.2,
+          },
+          scale: {
+            duration: 0.8,
+          },
+        }}
+      />
+    </div>
+  );
 }
